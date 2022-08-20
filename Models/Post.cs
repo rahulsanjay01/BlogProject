9cc034ad1efefe35e7d117051aeeacd92d720c76
rahulsanjay01 <90293567+rahulsanjay01@users.noreply.github.com>
2022-08-20 13:23:28 -0700

    added post model

---
+++ Models/Post.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Blog Name")]
+        public int BlogId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Abstract { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        public string Slug { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+    }
+}
