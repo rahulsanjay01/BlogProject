cd66cfbc697c1e84dbf4193e6f5c8b3467504aa9
rahulsanjay01 <90293567+rahulsanjay01@users.noreply.github.com>
2022-09-02 21:17:42 -0700

    email service

--- Program.cs
+++ Program.cs
@@ -1,19 +1,32 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogProject.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace BlogProject
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            //pull out my registered DataService
+            var dataService = host.Services
+                                  .CreateScope()
+                                  .ServiceProvider
+                                  .GetRequiredService<DataService>();
+
+            await dataService.ManageDataAsync();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
